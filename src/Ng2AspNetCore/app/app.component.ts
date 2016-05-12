7c8eadabaf7f4de297eb0f77826e5e667095a3eb
Jason Tucker <jatucke@sa28.net>
2016-05-12 11:51:23 -0400

    added the message list component. very basic and will be fleshed out shortly.

--- src/Ng2AspNetCore/app/app.component.ts
+++ src/Ng2AspNetCore/app/app.component.ts
@@ -1,7 +1,13 @@
-﻿import {Component} from '@angular/core';
+﻿import { Component } from '@angular/core';
+
+import { MessageListComponent } from './messages/message-list.component';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>SA28 Angular 2 App!</h1>'
+    directives: [MessageListComponent],
+    template: `
+        <h1>SA28 Angular 2 App!</h1>
+        <message-list></message-list>
+    `
 })
 export class AppComponent { }
---
+++ src/Ng2AspNetCore/app/messages/message-list.component.ts
@@ -0,0 +1,11 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: `message-list`,
+    templateUrl: `./app/messages/message-list.component.html`
+})
+export class MessageListComponent {
+
+    constructor() { }
+
+}
